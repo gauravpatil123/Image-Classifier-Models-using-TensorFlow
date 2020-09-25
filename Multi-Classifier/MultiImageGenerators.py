bcf39b4bb715e62acc997cbc1872e7097e58de89
gauravpatil123 <gaurav.patil123@gmail.com>
2020-09-25 14:59:15 +0530

    completed Multi-Classifier

--- Multi-Classifier/MultiImageGenerators.py
+++ Multi-Classifier/MultiImageGenerators.py
@@ -79,33 +79,7 @@ class Generators:
                                                                          class_mode=class_mode)
       return validation_generator
 
-
+# initializing the generators for training
 datagens = Generators()
 train_generator = datagens.create_train_generator(TRAINING_DIR, (300, 300), 50)
 validation_generator = datagens.create_val_generator(VALIDATION_DIR, (300, 300), 50)
-
-"""
-# defining train_datagen and validation_datagen with data augmentation
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   rotation_range = 40,
-                                   width_shift_range = 0.2,
-                                   height_shift_range = 0.2,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   fill_mode = 'nearest')
-
-validation_datagen = ImageDataGenerator(rescale = 1./255)
-
-# defining train and validation generators form datagens to flow images
-train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
-                                                    target_size = (300, 300),
-                                                    batch_size = 50,
-                                                    class_mode = 'categorical'
-                                                    )
-
-validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
-                                                              target_size = (300, 300),
-                                                              batch_size = 50,
-                                                              class_mode = 'categorical'
-                                                              )
-"""
